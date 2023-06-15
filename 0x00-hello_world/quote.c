3b1ab3a4bf8e4177b19a972fd3da47d26a6f52ef
levywan <levywanke@gmail.com>
2023-06-15 05:33:55 -0700

    hello c

--- 0x00-hello_world/quote.c
+++ 0x00-hello_world/quote.c
@@ -5,8 +5,8 @@
  *
  * Return: Always 1 (Success)
  */
-int main(void){
-        write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19",59);
+int main(void)
+{
+write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19",59);
 return (1);
 }
-
