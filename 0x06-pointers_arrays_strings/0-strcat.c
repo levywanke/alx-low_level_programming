e6427276ed17dd4e5cbb1b0d880ec83fc40bb87c
levywanke <levywanke@gmail.com>
2023-06-29 06:45:41 -0700

    concatenate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,15 +4,16 @@
  * _strcat - concatenates two strings
  * @dest: string to append to
  * @src: string to add
- *  
+ *
  * Return: a pointer to the resulting string
  */
 char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
-        i = 0;
+	i = 0;
 	j = 0;
+
 	while (dest[i] != '\0')
 		i++;
 
