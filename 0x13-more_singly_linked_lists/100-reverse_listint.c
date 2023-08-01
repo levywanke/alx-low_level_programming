b8845ef87f6e17c003567e4ae37fb43b930c0224
levywanke <levywanke@gmail.com>
2023-08-01 02:42:14 -0700

    reverse

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -13,10 +13,10 @@ listint_t *reverse_listint(listint_t **head)
 
 	while (*head)
 	{
-		next = *head->next;
-		*head->next = prev;
+		next = (*head)->next;
+		(*head)->next = prev;
 		prev = *head;
-		*head  = next;
+		*head = next;
 	}
 
 	*head = prev;
