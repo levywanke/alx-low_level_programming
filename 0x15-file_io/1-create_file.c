62e36e6ba0dec4cb6058b52a0c89b68ef9256ae3
levywanke <levywanke@gmail.com>
2023-08-07 11:50:57 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,15 +26,15 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		wb  = write(fd, text_content, len);
-		if (wb  == -1 || (size_t)wb != len)
-				{
-				close(fd);
-				return (-1);
-				}
+		wb = write(fd, text_content, len);
+		if (wb == -1 || (size_t)wb != len)
+		{
+			close(fd);
+			return (-1);
+		}
 
-				}
+	}
 
-				close(fd);
-				return (1);
-				}
+	close(fd);
+	return (1);
+}
